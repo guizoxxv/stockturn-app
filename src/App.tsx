ea13003ad0c3f3afec07888168ea8228ebb9eeae
guizoxxv <guizoxxv@gmail.com>
2021-02-27 22:45:28 -0300

    Established communication with backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App: React.FC = () => {
+  useEffect(() => {
+    axios.get('http://api.local.test:8001/sanctum/csrf-cookie').then(res => {
+      console.log(res);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Opa!</h1>
     </div>
   );
 }
