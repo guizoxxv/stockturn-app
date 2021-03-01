2405541c0ac20635b5a4a6ba644187d2689ba29a
guizoxxv <guizoxxv@gmail.com>
2021-02-28 21:19:00 -0300

    Auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,54 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
+axios.defaults.withCredentials = true;
+
+const apiBaseUrl = 'http://api.local.test:8001';
+
 const App: React.FC = () => {
-  useEffect(() => {
-    axios.get('http://api.local.test:8001/sanctum/csrf-cookie').then(res => {
-      console.log(res);
-    });
-  }, []);
+  const [logged, setLogged] = useState(false);
+  const [msg, setMsg] = useState('');
+
+  const handleLogout = () => {
+    axios.post(apiBaseUrl + '/logout')
+      .then(res => {
+        setLogged(false);
+      })
+      .catch(err => console.log('[POST /logout]', err));
+  }
+
+  const handleLogin = () => {
+    axios.get(apiBaseUrl + '/sanctum/csrf-cookie')
+      .then(res => {
+        axios.post(apiBaseUrl + '/login', {
+          email: 'admin@example.com',
+          password: 'secret123',
+        })
+          .then(res => {
+            setLogged(true);
+
+            axios.get(apiBaseUrl + '/api/user')
+              .then(res => {
+                alert('Worked');
+              })
+              .catch(err => {
+                console.log('[GET /api/user]', err);
+                setMsg('Fail to get logged user');
+              });
+          })
+          .catch(err => console.log('[POST /login]', err));
+      })
+      .catch(err => console.log('[GET /sanctum/csrf-cookie]', err));
+  }
 
   return (
     <div className="App">
-      <h1>Opa!</h1>
+      <p>Is logged in? <b>{logged ? 'YES' : 'NO'}</b></p>
+      <button onClick={handleLogin} style={{marginRight:'3px'}}>LOGIN</button>
+      <button onClick={handleLogout}>LOGOUT</button>
+      {msg && (
+        <p><b>Message:</b> {msg}</p>
+      )}
     </div>
   );
 }
